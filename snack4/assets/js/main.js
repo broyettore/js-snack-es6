1431e9c5995d12be4f50a90aa05da00794ea3ce6
broyettore <broyettore@gmail.com>
2023-03-09 17:59:24 +0100

    snack4 done

--- snack4/assets/js/main.js
+++ snack4/assets/js/main.js
@@ -1,10 +1,75 @@
 'use strict';
 
-// SNACK 4
-// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
-// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
-// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+/******* 
+ * FUNCTIONS 
+ * ***** */
+
+
+function randomNum(num) {
+    const rand = Math.floor((Math.random() * num) + 1)
+    return rand;
+} 
+
+/************ VARIABLES ************ */
+const container = document.querySelector(".list-ctn");
+const randGoals = randomNum(50);
+
+const footballStats = [
+    {
+        name: "Real Madrid",
+        goalsMade: 0,
+        goalsTaken: 0
+    },
+
+    {
+        name: "Juventus",
+        goalsMade: 0,
+        goalsTaken: 0
+    },
+
+    {
+        name: "Psg",
+        goalsMade: 0,
+        goalsTaken: 0
+    },
+
+    {
+        name: "Chelsea",
+        goalsMade: 0,
+        goalsTaken: 0
+    },
+
+    {
+        name: "Liverpool",
+        goalsMade: 0,
+        goalsTaken: 0
+    }
+]
+
+const newfootballStats = footballStats.map(element => {
+    const {name} = element;
+    element.goalsMade = randGoals;
+    element.goalsTaken = randGoals;
+
+    return {name: name, goalsTaken: element.goalsTaken};
+})
+
+for (let i = 0; i < footballStats.length; i++) {
+    const listEl = document.createElement("li");
+    listEl.classList.add("item")
+    container.append(listEl);
+
+    const listElSelect = document.querySelectorAll(".item")
+
+    listElSelect[i].append(newfootballStats[i].name + " wtih "+ newfootballStats[i].goalsTaken+ " goals taken")
+    
+}
+console.log(newfootballStats)
+
+
+
+
+
 
 
 
